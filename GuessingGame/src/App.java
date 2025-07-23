a93115eba9d9718b66942903a8f75a356a49db93
adhi13j <jsrivenkata13@gmail.com>
2025-07-23 13:51:13 +0530

    made a GuessingGame

--- GuessingGame/src/App.java
+++ GuessingGame/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("give a random numeber between 1 and 10 inclusive");
+        String answer = Integer.toString((int)(Math.random()*10 + 1));
+
+        String message = sc.nextLine() ;
+        while (!message.equals("end") && !message.equals(answer)){
+            System.out.println("Wrong answer");
+            System.out.println("give a random numeber between 1 and 10 inclusive");
+            message = sc.nextLine() ;
+        }
+        sc.close();
+        if (message != "end"){System.out.println("correct the answer was " + answer);return;}
+        System.out.println("you have given up");
+        return;
     }
 }
