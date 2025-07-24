fa69f9511ae22688fc2e0a2388e76b9e13a1b6d3
adhi13j <jsrivenkata13@gmail.com>
2025-07-24 19:39:22 +0530

    finally understood java fx using gradle

--- TodoList/app/src/main/java/todo/MainApp.java
+++ TodoList/app/src/main/java/todo/MainApp.java
@@ -1,27 +1,53 @@
-package todo;
+package todo ;
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.shape.Circle;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class MainApp extends Application {
     @Override
-    public void start(Stage stage) {
-        Circle circle = new Circle();
-        circle.setCenterX(300.0f);
-        circle.setCenterY(135.0f);
-        circle.setRadius(100.0f);
-        Group root = new Group(circle);
-        Scene scene = new Scene(root, 600, 300);
-        stage.setTitle("JavaFX Circle");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        GridPane grid = new GridPane();
+        //grid.setHgap(10);
+        //grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setGridLinesVisible(true);
+
+        Label userNameLabel = new Label("User Name:");
+        TextField userNameField = new TextField();
+        Label passwordLabel = new Label("Password:");
+        TextField passwordField = new TextField();
+
+        Button loginBtn = new Button("Sign in");
+        loginBtn.setOnAction(event -> {
+            String username = userNameField.getText();
+            String password = passwordField.getText();
+            if (username.isEmpty() || password.isEmpty()) {
+                System.out.println("Please fill in all fields.");
+            } else {
+                System.out.println("Login attempted with username: " + username);
+            }
+        });
+
+        grid.add(userNameLabel, 0, 0);
+        grid.add(userNameField, 1, 0);
+        grid.add(passwordLabel, 0, 1);
+        grid.add(passwordField, 1, 1);
+        grid.add(loginBtn, 1, 2);
+
+        Scene scene = new Scene(grid, 300, 200);
+        primaryStage.setTitle("Login Form");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        
         launch(args);
     }
 }
